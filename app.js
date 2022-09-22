cc1c0244506bab3852102ff51811eb6531e736b7
Fabian <fabianvelazquez2196@gmail.com>
2022-09-22 01:38:53 -0300

    mi primer commit

---
+++ app.js
@@ -0,0 +1,35 @@
+// CHEQUEANDO GIT
+
+/* BASIC COMANDS
+
+- git init         ----->   inicializar git (un repositorio) en el proyecto, sea un proyecto nuevo o no
+- got add <file>     ----->  pasa los archivos del Working directory la Staging area                                       
+- git status     ----->    para ver en que estado esta nuestro archivo, si esta en el Working directory, Staging area, etc.     
+- git commit    ----->   para pasarlos del Staging area al Repositorio, primer snapshot 
+- git push     ----->     para subirlo a un repositorio remoto 
+- git pull       ----->    para bajar el codigo
+- git clone        ----->   hace una copia de el repositorio desde el servidor central a tu servidor
+
+Los 3 estadis de Git 
+
+Working directory -----> Staging area -----> Repository
+
+
+COMANDOS RANDOM 
+
+- ls  -----> comando para listar directorios y archivos
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+
+console.log ("Hola Mundo")
